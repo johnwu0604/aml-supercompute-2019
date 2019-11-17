5ab4c7549bb6a4335bd10353cc07b9d87d6500b4
John Wu <johnwu0604@gmail.com>
2019-11-16 18:53:32 -0800

    Added score script

--- score.py
+++ score.py
@@ -1,61 +1,32 @@
-# USAGE
-# python score.py --model athletes.model --labelbin lb.pickle --image examples/charmander_counter.png
-
-# import the necessary packages
-from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-import numpy as np
-import argparse
-import imutils
-import pickle
-import cv2
 import os
+import cv2
+import json
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="path to trained model model")
-ap.add_argument("-l", "--labelbin", required=True,
-	help="path to label binarizer")
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
+def init():
+	model_dir = 'outputs'
+	global model 
+	model = load_model(os.path.join(model_dir, 'model.h5'))
 
-# load the image
-image = cv2.imread(args["image"])
-output = image.copy()
+def run():
+	image_dim = 250
+	image_url = json.loads(raw_data)['image_url']
+	face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+	image = cv2.imread(image_url)
+	detected_face = face_classifier.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1, minSize=(1,1))
+	x = detected_face[0][0]
+	y = detected_face[0][1]
+	l = detected_face[0][2]
+	w = detected_face[0][3]
+	image = image[y:y+l, x:x+w]
+	image = cv2.resize(image, (image_dim, image_dim))
+	image = np.float32(image)
+	pred = model.predict([[image]])
+	return pred
 
-# pre-process the image for classification
-image = cv2.resize(image, (256, 256))
-image = image.astype("float") / 255.0
-image = img_to_array(image)
-image = np.expand_dims(image, axis=0)
+init()
+result = run(json.dumps({
+	'image_url': 'test/lebron.jpg'
+}))
 
-# load the trained convolutional neural network and the label
-# binarizer
-print("[INFO] loading network...")
-model = load_model(args["model"])
-lb = pickle.loads(open(args["labelbin"], "rb").read())
-
-# classify the input image
-print("[INFO] classifying image...")
-proba = model.predict(image)[0]
-idx = np.argmax(proba)
-label = lb.classes_[idx]
-
-# we'll mark our prediction as "correct" of the input image filename
-# contains the predicted label text (obviously this makes the
-# assumption that you have named your testing image files this way)
-filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
-correct = "correct" if filename.rfind(label) != -1 else "incorrect"
-
-# build the label and draw the label on the image
-label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
-output = imutils.resize(output, width=400)
-cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (0, 255, 0), 2)
-
-# show the output image
-print("[INFO] {}".format(label))
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+print(result)
